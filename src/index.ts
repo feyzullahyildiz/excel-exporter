812b382fa5bda0bc327364295a953fe6abae10d8
Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2020-01-02 11:27:06 +0300

    unknown

--- src/index.ts
+++ src/index.ts
@@ -17,67 +17,18 @@ if (fs.existsSync(filename)) {
 }
 
 
-// const workbook = new ExcelJS.stream.xlsx.WorkbookWriter(excelFileOptions);
 const workbook = new ExcelJS.Workbook();
 const writeToExcel = fs.createWriteStream(filename);
 
-// create a sheet with red tab colour
-// var sheet = workbook.addWorksheet('My Sheet', { properties: { tabColor: { argb: 'FFC0000' } } });
-
-// create a sheet where the grid lines are hidden
-const worksheet = workbook.addWorksheet('My Sheet', { properties: { showGridLines: false } });
+const worksheet = workbook.addWorksheet('1', { properties: { showGridLines: false } });
 worksheet.getCell('A1').value = (Math.random() * 100).toFixed(0)
-// create a sheet with the first row and column frozen
-// var sheet = workbook.addWorksheet('My Sheet', { views: [{ xSplit: 1, ySplit: 1 }] });
-
-// const columns = [
-//     { header: 'İsim', key: 'name' },
-//     { header: 'Soyisim', key: 'surname' },
-// ]
-
-// sheet.columns = columns;
-
-// const row = sheet.addRow({name: 'Feyz', surname: 'YILDIZ'})
-// row.commit()
 worksheet.views = [
     { state: 'frozen', xSplit: 3, ySplit: 7, activeCell: 'A1' }
 ]
-// worksheet.mergeCells('A8:A15');
-
-// const columnA = worksheet.getColumn('A')
-// columnA.width = 3
-// const cellA8 = worksheet.getCell('A8');
-// setCellFontGray(cellA8);
-// setCellBackgroundGray(cellA8)
-// addDefaultDarkBorder(cellA8)
-// rotateCell(cellA8)
-// setCellFontBold(cellA8)
-// cellA8.value = 'SABAH'
 
 drawDataModal(worksheet, worksheet.getCell('A6'), demoData);
-drawDataModal(worksheet, worksheet.getCell('A24'), demoData);
+drawDataModal(worksheet, worksheet.getCell('A24'), Object.assign(demoData, {displayName: 'AKŞAM'}));
 
-// const table = worksheet.addTable({
-//     name: 'MyTable',
-//     ref: 'B8',
-//     headerRow: true,
-//     totalsRow: true,
-//     style: {
-//         theme: 'TableStyleDark3',
-//         showRowStripes: true,
-//     },
-//     columns: [
-//         { name: 'Date', totalsRowLabel: 'Totals:', filterButton: true },
-//         { name: 'Amount', totalsRowFunction: 'sum', filterButton: false },
-//     ],
-//     rows: [
-//         [new Date('2019-07-20'), 70.10],
-//         [new Date('2019-07-21'), 70.60],
-//         [new Date('2019-07-22'), 70.10],
-//     ],
-// });
-// sheet.commit();
-// workbook.commit();
 workbook.xlsx.write(writeToExcel).then(a => {
     console.log('finitto')
 })
--- src/utils.ts
+++ src/utils.ts
@@ -29,9 +29,54 @@ export function rotateCell(cell: ExcelJS.Cell) {
         horizontal: 'centerContinuous',
     };
 }
+export function getRefCell(cell: ExcelJS.Cell, x: number, y: number) {
+    const sheet = cell.worksheet;
+    const [row, col] = getCellPosition(cell)
+    return sheet.getCell(row + y, col + x);
+}
+export function setCellFontCenter(cell: ExcelJS.Cell) {
+    if (cell.alignment) {
+        cell.alignment.vertical = 'middle';
+        cell.alignment.horizontal = 'center';
+
+    } else {
+        cell.alignment = {
+            vertical: 'middle',
+            horizontal: 'center',
+        }
+    }
+}
 export function setCellFontGray(cell: ExcelJS.Cell) {
-    cell.font = {
-        color: { argb: Color.BLACK }
+    if(cell.font) {
+        cell.font.color = { argb: Color.BLACK }
+    } else {
+        cell.font = {
+            color: { argb: Color.BLACK }
+        }
+    }
+}
+export function setCellFontSelectedFont(cell: ExcelJS.Cell) {
+    if(cell.font) {
+        cell.font.name = 'Times New Roman'
+    } else {
+        cell.font = {
+            name: 'Times New Roman'
+        }
+    }
+}
+type BorderSide = 'top' | 'left' | 'bottom' | 'right'
+export function addDarkBorder(cell: ExcelJS.Cell, ...sides: BorderSide[]) {
+    if (cell.border) {
+        for (const side of sides) {
+            cell.border[side] = { style: 'medium' }
+        }
+
+    } else {
+        cell.border = {}
+        for (const side of sides) {
+            cell.border[side] = { style: 'medium' }
+
+        }
     }
 }
 export function addDefaultDarkBorder(cell: ExcelJS.Cell) {
@@ -47,6 +92,15 @@ export function setCellBackgroundGray(cell: ExcelJS.Cell) {
         type: 'pattern',
         pattern: 'darkVertical',
         fgColor: { argb: Color.GRAY }
+    }
+}
+export function setCellFontItalic(cell: ExcelJS.Cell) {
+    if (cell.font) {
+        cell.font.italic = true
+    } else {
+        cell.font = {
+            italic: true,
+        }
     }
 }
 export function setCellFontBold(cell: ExcelJS.Cell) {
@@ -209,17 +263,162 @@ export const demoData: DataModal = {
         },
     ]
 }
+const getCellPosition = (cell: ExcelJS.Cell): number[] => {
+    const row: number = cell.fullAddress.row as any;
+    const col: number = cell.fullAddress.col as any;
+    return [row, col]
+}
+const setDayPeriodCell = (dayPeriodCell: ExcelJS.Cell, dataModal: DataModal) => {
+    const sheet = dayPeriodCell.worksheet;
+    const [row, col] = getCellPosition(dayPeriodCell);
+    const endOfDayPeriodCell = sheet.getCell(row + dataModal.rowCount - 1, col);
+    sheet.mergeCells(dayPeriodCell.address, endOfDayPeriodCell.address)
+    setCellFontGray(dayPeriodCell);
+    setCellBackgroundGray(dayPeriodCell)
+    addDefaultDarkBorder(dayPeriodCell)
+    rotateCell(dayPeriodCell)
+    setCellFontBold(dayPeriodCell)
+    dayPeriodCell.value = dataModal.displayName
+}
+const setColWidthHeighValues = (startCell: ExcelJS.Cell) => {
+    const sheet = startCell.worksheet;
+    const [row, col] = getCellPosition(startCell);
+    sheet.getColumn(col).width = 3;
 
-export const drawDataModal = (sheet: ExcelJS.Worksheet, startCell: ExcelJS.Cell, dataModal: DataModal) => {
-    const { row, col } = startCell.fullAddress;
-    const titleCell = sheet.getCell()
+    sheet.getColumn(col + 1).width = 5;
+    sheet.getColumn(col + 2).width = 5;
+
+    sheet.getRow(row).height = 25
+}
+
+const setTimeValues = (startCell: ExcelJS.Cell, dataModal: DataModal) => {
+    const sheet = startCell.worksheet
+    // const [sRow, sCol] = getCellPosition(startCell);
+    // const row = sRow + 2
+    // const col = sCol + 1
+    const [row, col] = getCellPosition(startCell);
+    dataModal.times.forEach((time, index) => {
+
+        const timeStartRow = row + index
+        const timeStartCol = col
+        const startCell = sheet.getCell(timeStartRow, timeStartCol)
+        startCell.value = time.start
+        setCellFontCenter(startCell)
+
+        const timeEndRow = row + index
+        const timeEndCol = col + 1
+        const endCell = sheet.getCell(timeEndRow, timeEndCol);
+        setCellFontCenter(endCell)
+        endCell.value = time.end
+    })
+
+}
+const drawVehicleRaportItem = (startCell: ExcelJS.Cell, item: VehicleRaportItem) => {
+    const sheet = startCell.worksheet;
+    const [row, col] = getCellPosition(startCell);
+    const titleEndCell = sheet.getCell(row, col + item.subHeaderCount - 1);
+    sheet.mergeCells(startCell.address, titleEndCell.address)
+    startCell.value = item.header.displayName
+    setCellFontCenter(startCell)
+    addDefaultDarkBorder(startCell)
+    const getColIndexOfCol = (key: string): number => {
+        return item.subHeaders.findIndex(a => a.key === key)
+    }
+    item.subHeaders.forEach((header, index) => {
+        const cell = getRefCell(startCell, index, 1);
+        const columnObject = sheet.getColumn(cell.col)
+        columnObject.width = 5;
+        cell.value = header.displayName
+        setCellFontCenter(cell);
+        addDefaultDarkBorder(cell);
+
+        const countResultStartCell = getRefCell(startCell, index, 1);
+        const countResultEndCell = getRefCell(startCell, index, item.rows.length + 1);
+        const totalResultCell = getRefCell(startCell, index, item.rows.length + 2);
+        setCellFontCenter(totalResultCell)
+        addDefaultDarkBorder(totalResultCell)
+        totalResultCell.value = {
+            formula: `SUM(${countResultStartCell.address}:${countResultEndCell.address})`,
+            date1904: true
+        }
+        setCellBackgroundGray(totalResultCell);
+        setCellFontItalic(totalResultCell)
+    })
+    // Her Saate karşılık gelen data bunun içinde
+    item.rows.forEach((row, rowIndex) => {
+        const keys = Object.keys(row)
+        for (const key of keys) {
+            const index = getColIndexOfCol(key)
+            if (index !== -1) {
+                const cell = getRefCell(startCell, index, rowIndex + 2)
+                setCellFontCenter(cell)
+                cell.value = row[key];
+                if (index === 0) {
+                    addDarkBorder(cell, 'left')
+                } else if (index === item.subHeaderCount - 1) {
+                    addDarkBorder(cell, 'right')
+                }
+            }
+        }
+    });
+}
+const setTimeCellHeader = (startCell: ExcelJS.Cell) => {
+    const sheet = startCell.worksheet;
+    const [row, col] = getCellPosition(startCell);
+    const titleCell = sheet.getCell(row, col + 1)
+    const endOfTitleCell = sheet.getCell(row + 1, col + 2)
+    sheet.mergeCells(titleCell.address, endOfTitleCell.address)
+
     setCellFontGray(titleCell);
-    setCellBackgroundGray(titleCell)
     addDefaultDarkBorder(titleCell)
-    rotateCell(titleCell)
     setCellFontBold(titleCell)
-    const mergedTimeCellValue = `${col}${row}`
+    setCellFontCenter(titleCell)
+    titleCell.value = `ÇEKİM\nSAATİ`
+}
+
+const setTotalCellFooter = (startCell: ExcelJS.Cell) => {
+    const sheet = startCell.worksheet;
+    const [row, col] = getCellPosition(startCell);
+    const totalCell = sheet.getCell(row, col)
+    const endOfTotalCell = getRefCell(totalCell, 1, 0);
+    sheet.mergeCells(totalCell.address, endOfTotalCell.address);
+    addDefaultDarkBorder(totalCell);
+    setCellBackgroundGray(totalCell)
+    setCellFontItalic(totalCell)
+    setCellFontBold(totalCell)
+    setCellFontCenter(totalCell)
+    totalCell.value = 'TOPLAM';
+}
+const setWorkSheetFont = (sheet: ExcelJS.Worksheet) => {
+    let i = 0;
+    sheet.eachRow(row => {
+        row.eachCell(cell => {
+            if(cell.value !== undefined) {
+                setCellFontSelectedFont(cell)
+                i++;
+            }
+        })
+    })
+    console.log('setCellFontSelectedFont CELL', i)
+}
+export const drawDataModal = (sheet: ExcelJS.Worksheet, startCell: ExcelJS.Cell, dataModal: DataModal) => {
+    const [row, col] = getCellPosition(startCell);
+    setColWidthHeighValues(startCell);
+    setDayPeriodCell(getRefCell(startCell, 0, 2), dataModal)
+    setTimeCellHeader(startCell)
+    setTimeValues(getRefCell(startCell, 1, 2), dataModal)
+    setTotalCellFooter(getRefCell(startCell, 1, dataModal.rowCount + 2))
+    const drawVehicleRaportItemStartCell = getRefCell(startCell, 3, 0);
+    let xOffsetValue = 0;
+    dataModal.data.forEach((vehicleModal) => {
+        const refCell = getRefCell(drawVehicleRaportItemStartCell, xOffsetValue, 0)
+        drawVehicleRaportItem(refCell, vehicleModal)
+        xOffsetValue += vehicleModal.subHeaderCount
+    })
+    setWorkSheetFont(sheet)
+    // const mergedTimeCellValue = `${col}${row}`
     // // sheet.mergeCells()
-    // console.log('Row', row)
+
+    console.log('Row', row)
 
 }
